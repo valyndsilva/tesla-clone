9deaa6a1fcc30a3c45877109bc677985d7e2d1ff
valyndsilva <valyndsilva@gmail.com>
2023-02-24 13:14:11 +0000

    types deps

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,6 +1,6 @@
 import { ExpandMore } from "@mui/icons-material";
 import React from "react";
-import Fade from "react-reveal/Fade";
+// import Fade from "react-reveal/Fade";
 
 function Section({
   title,
@@ -15,14 +15,14 @@ function Section({
       id={id}
       className={`flex flex-col justify-between items-center w-[100vw] h-[100vh] bg-cover bg-center bg-no-repeat !bg-[url('/assets/${backgroundImg}')]`}
     >
-      <Fade bottom>
+      {/* <Fade bottom> */}
         <div className="text-center pt-[15vh]">
           <h1 className="text-4xl">{title}</h1>
           <p>{description}</p>
         </div>
-      </Fade>
+      {/* </Fade> */}
       <div>
-        <Fade bottom>
+        {/* <Fade bottom> */}
           <div className="flex flex-col md:flex-row mb-8">
             <div className="bg-gray-600 h-10 w-[256px] text-white flex items-center justify-center rounded-sm opacity-85 uppercase text-sm cursor-pointer m-[8px]">
               {leftBtnText}
@@ -33,7 +33,7 @@ function Section({
               </div>
             )}
           </div>
-        </Fade>
+        {/* </Fade> */}
         <div className="flex w-full items-center justify-center  h-[40px] animate-animateDown overflow-x-hidden text-white">
           <ExpandMore className="w-5 h-5 !text-white" />
         </div>
